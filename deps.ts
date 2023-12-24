80da53144e46ee550cc5f7a420a51cda350c1372
Callum Macdonald <git@callum-macdonald.com>
2023-12-24 15:46:33 +0100

    Working prototype

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export * as fs from "https://deno.land/std@0.210.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.210.0/path/mod.ts";
+export * as cliffy from "https://deno.land/x/cliffy@v0.25.7/mod.ts";
---
+++ main.ts
@@ -0,0 +1,36 @@
+import { cliffy, fs, path } from "./deps.ts";
+
+await new cliffy.Command()
+  .name("d2folders")
+  .version("0.1.0")
+  .option("--dry-run", "Do not rename any files")
+  .option("-d --debug", "Output debugging information")
+  .arguments("<path:string>")
+  .action(async (options, dendronPath: string) => {
+    const pathGlob = path.join(dendronPath, "*.md");
+
+    for await (const file of fs.expandGlob(pathGlob)) {
+      if (!file.isFile || file.name.slice(-3) !== ".md") {
+        continue;
+      }
+
+      const baseName = path.basename(file.name, ".md");
+      const newBaseName = baseName.replaceAll(".", "/");
+      const newFileName = newBaseName + ".md";
+      const newFileNameDirName = path.dirname(newFileName);
+      const newDirPath = path.join(dendronPath, newFileNameDirName);
+      const newFilePath = path.join(dendronPath, newFileName);
+
+      if (options.debug) {
+        console.log(`${file.path} -> ${newFileName} in ${newDirPath}`);
+      }
+
+      if (options.dryRun) {
+        continue;
+      }
+
+      await fs.ensureDir(newDirPath);
+      await Deno.rename(file.path, newFilePath);
+    }
+  })
+  .parse();
